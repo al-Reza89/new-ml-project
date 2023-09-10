742b22de32a670e1607038f1dd52d158de6dfbe4
al-Reza89 <alialreza98@gmail.com>
2023-09-10 22:30:43 +0600

    add instruction

--- app/components/homePage/HomePage.tsx
+++ app/components/homePage/HomePage.tsx
@@ -6,7 +6,7 @@ import { Collection } from "@prisma/client";
 import axios from "axios";
 import { toast } from "react-hot-toast";
 import { useRouter } from "next/navigation";
-import { PulseLoader } from "react-spinners";
+import { PulseLoader, ClipLoader } from "react-spinners";
 
 const OPTIONS = [
   "Fiction",
@@ -16,6 +16,7 @@ const OPTIONS = [
   "Tragedy",
   "Thriller",
   "Motivational",
+  "Romantic",
 ];
 
 interface HomePageProps {
@@ -46,6 +47,7 @@ const HomePage: React.FC<HomePageProps> = ({ mlData, userId }) => {
         Tragedy: 0,
         Thriller: 0,
         Motivational: 0,
+        Romantic: 0,
         isValidate: 1,
         userId: userId,
       };
@@ -75,6 +77,47 @@ const HomePage: React.FC<HomePageProps> = ({ mlData, userId }) => {
 
   return (
     <div className=" flex flex-col gap-5">
+      <div>
+        <div className="  collapse collapse-plus border border-base-300 bg-base-200">
+          <input type="checkbox" />
+          <div className="collapse-title text-xl font-medium">Instructions</div>
+          <div className="collapse-content bg-base-100 ">
+            <p className="pt-4">Please select multiple category </p>
+            <p>
+              1. কাব্য (Fiction): এটি কোনো বাস্তব কাহিনীর বর্ণ্না নয়, বরং
+              কল্পনামূলক ঘটনা ও চরিত্রের সৃষ্টির মাধ্যমে তৈরি হওয়া গল্প।
+            </p>
+            <p>
+              2. অকাব্য (Non-fiction): এটি কল্পনা নয়, প্রবৃত্তির-বাস্ততবে ঘটেছে
+              এমন উপর ভিত্তি করে লেখা, যা সত্যকথা ও তথ্যে ভরপূর্ণ।
+            </p>
+            <p>
+              3. নাটক (Drama): বর্ণ্না ভিত্তিক নাট্য যাতে একটি কাহিনী বা ঘটনা
+              নায়ক-নায়িকার মাধ্যমে প্রদর্শন করা হয়।
+            </p>
+            <p>
+              4. রহস্য (Mystery): গল্প কোনো অদ্ভুত ও সমাধান করা কঠিন ঘটনার
+              চরিত্রের উপর ভিত্তি করা, যা পাঠকের প্রশ্ন ও চিন্তা সৃজন করে।
+            </p>
+            <p>
+              5. বিপত্তি (Tragedy): যেখানে প্রধান চরিত্রের অসুস্থি ও কাহিনীর শেষ
+              দিকে দু:খের সমাপ্তি সংঘটিত হয়।
+            </p>
+            <p>
+              6. থ্রিলার (Thriller): ঘটনা চরম সতর্কতা, গল্পের গভীরতার সাথে গতি
+              বা উত্তেজনা সৃষ্টি করে, পাঠকের কৌতুহল আর উদ্দীপনা তৈরি করে।
+            </p>
+            <p>
+              7. উৎসাহপ্রদ (Motivational): অভিজ্ঞতা ও উৎসাহের মাধ্যমে পাঠককে
+              উৎসাহিত করে এবং সাফল্যের দিকে অগ্রসর হতে প্রেরণা দেয়।
+            </p>
+            <p>
+              8.প্রেমমূলক(Romantic) : গল্প মূলত প্রেম-ভালোবাসা ভিত্তিক করে লিখা
+              , যেখানে নায়ক-নায়িকার প্রেম-ভালোবাসার বর্ণ্না গুলা মূখ্য থাকে ।
+            </p>
+          </div>
+        </div>
+      </div>
       <div>
         <div className="  collapse collapse-plus border border-base-300 bg-base-200">
           <input type="checkbox" />
@@ -111,7 +154,7 @@ const HomePage: React.FC<HomePageProps> = ({ mlData, userId }) => {
           <div className="text-gray-400  ">
             Please Select Multiple Category:
           </div>
-          <div className="pt-4  md:flex md:items-center gap-4 ">
+          <div className="pt-4   md:flex md:items-center md:justify-center gap-4 ">
             <Select
               mode="multiple"
               placeholder="Inserted are removed"
@@ -129,7 +172,8 @@ const HomePage: React.FC<HomePageProps> = ({ mlData, userId }) => {
               <button
                 className={`bg-green-900 h-10 w-full md:w-24 mt-6 md:gap-5 md:mt-0 rounded-sm text-white font-bold text-lg px-2 py-1 cursor-not-allowed `}
               >
-                <PulseLoader color="#f8f6fc" />
+                {/* <PulseLoader color="#f8f6fc" /> */}
+                <ClipLoader color="#f8f6fc" size={25} />
               </button>
             ) : (
               <button
